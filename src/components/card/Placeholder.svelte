d5de777841ae40016d7c3ce603c48989fbe066e6
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-02-24 09:08:15 -0500

    set dpr default and added zoomout to options

--- src/components/card/Placeholder.svelte
+++ src/components/card/Placeholder.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { tv } from "tailwind-variants";
   const placeholder = tv({
-    base: "absolute inset-0 block bg-[image:var(--placeholder)] bg-[50%_45%] bg-no-repeat @landscape:bg-[size:35cqh_35cqh] @portrait:bg-[size:35cqw_35cqw] @square:bg-[size:35cqw_35cqw]",
+    base: "absolute inset-0 block bg-[image:var(--placeholder)] bg-[50%_45%] bg-no-repeat @landscape:bg-[size:35cqh_35cqh] @portrait:bg-[size:45cqw_45cqw] @square:bg-[size:35cqw_35cqw]",
     variants: {
       hasSplash: {
         true: "hidden",
